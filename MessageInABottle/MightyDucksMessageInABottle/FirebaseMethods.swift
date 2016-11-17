64a0272d7d1313c523a86d4d9f26b4d80f75f46f
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-11-17 13:30:08 -0500

    updated firebase methods outline

---
+++ MessageInABottle/MightyDucksMessageInABottle/FirebaseMethods.swift
@@ -0,0 +1,46 @@
+//
+//  FirebaseMethods.swift
+//  MightyDucksMessageInABottle
+//
+//  Created by Felicity Johnson on 11/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+
+class FirebaseMethods {
+
+/*
+ Sending data TO Firebase:
+ 
+ 1. func to add 2 users to specific chat
+ 
+ 2. func to remove user from specific chat
+ 
+ 3. create new user
+ 
+ 4. create new chat
+ 
+ 5. add message to chat
+ 
+ 6. create bottle message for specific ocean
+ 
+ */
+
+
+/*
+ Retreiving data FROM Firebase:
+ 
+ 1. append messages for each user to [Message]() that will be dislpayed on table view
+ 
+ 2. append messages to [Message]() in ocean in tableView
+ 
+ 3. retreive user info of given user / self
+ 
+ 
+ */
+ 
+ 
+ 
+ }
---
+++ MessageInABottle/Pods/Target Support Files/Pods-MightyDucksMessageInABottle/Pods-MightyDucksMessageInABottle-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MightyDucksMessageInABottle : NSObject
+@end
+@implementation PodsDummy_Pods_MightyDucksMessageInABottle
+@end
